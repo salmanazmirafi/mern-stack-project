72da659967e5d12fa0fc3b6dd28bab410de13542
salmanazmirafi <116106498+salmanazmirafi@users.noreply.github.com>
2022-12-13 21:57:09 +0600

    Update

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-hdi,dk
+import express from 'express'
+const app = express()
+import mysql from 'mysql2'
+
+//============database==================
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"salmanAzImrafi0394?dMD",
+    database:"text"
+})
+
+//=============all routes===============
+app.get('/', (req,res)=>{
+    res.send("hi")
+})
+app.get('/books', (req,res)=>{
+   const q = "SELECT * FROM books"
+   db.query(q,(err,data)=>{
+    if (err) return res.json(err)
+    return res.json(data)
+   })
+})
+
+
+//=============server run===============
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
