b9fbd4285cefd80708284de0fc64a6d07c8db2d6
salmanazmirafi <116106498+salmanazmirafi@users.noreply.github.com>
2022-12-26 23:22:45 +0600

    Update

--- api/middleware/error.js
+++ api/middleware/error.js
@@ -1,16 +1,16 @@
-const ErrorHandler = require("../utils/errorhander")
+const ErrorHandler = require("../utils/errorhander");
 
-module.exports = (err,req,res,next)=>{
-    err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal Server Error"
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
 
-// Wrong Mongodb Id Error
-if(err.name === "CastError"){
-  const message =  `Resource not found. Invalid: ${err.path}`;
-  err = new ErrorHandler(message, 400)
-}
-    res.status(err.statusCode).json({
-        success: false,
-        message: err.message,
-      });
-}
+  // Wrong Mongodb Id Error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
